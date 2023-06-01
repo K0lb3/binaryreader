20ecfddbe3f055be94a560bab241e561f7d65e96
Rudolf Kolbe <rkolbe96@gmail.com>
2023-06-01 23:23:58 +0200

    include <include\cpython\floatobject.h>

--- binaryreader.c
+++ binaryreader.c
@@ -1,6 +1,6 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-
+#include <floatobject.h>
 /*  
 ############################################################################
     int type definitions
